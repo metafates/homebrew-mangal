b9530d6d0fa62dcf9d78badad42eddaabb8c0383
Vadim <fates@duck.com>
2022-06-27 09:33:55 +0300

    update mangal

--- mangal.rb
+++ mangal.rb
@@ -2,8 +2,8 @@
 class Mangal < Formula
     desc "The ultimate CLI manga downloader"
     homepage "https://github.com/metafates/mangal"
-    url "https://github.com/metafates/mangal", :using => :git, :tag => "v1.5.2"
-    version "1.5.2"
+    url "https://github.com/metafates/mangal", :using => :git, :tag => "v2.0.0"
+    version "2.0.0"
     sha256 "92da0f4a880f86a5d782f47cf912f0206e2d49c5fcc27d57931ce0ef96e85029"
     license "MIT"
 
